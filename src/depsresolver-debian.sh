5630d976553dd7964228d0ad3ac6b0c730aa7c0f
David <dontcallmebrownie@gmail.com>
2025-10-08 11:15:49 -0500

    start depends reslover script for debian systems

---
+++ src/depsresolver-debian.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#########################################
+#                  TODO                 #
+#########################################
+#
+#   1) Show Welcome Message
+#   2) Show name of script
+#   3) Show the date and time
+#   4) Wait for user to start
+#   5) Install deps
+#   6) Show goodbye message
+#   7) Wait for user to Close
+#########################################
+
+echo "Project Rhapsody Depenency Script\n"
+echo "\tOnly for Debian Systems\n"
+echo "\t(for now)"

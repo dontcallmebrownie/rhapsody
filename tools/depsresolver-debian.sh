9e85099c1e5cf1dc73a254e64fee9543ff9398ee
David Cramer <dontcallmebrownie@gmail.com>
2025-10-08 13:56:49 -0500

    initial boot files added + FAT applied to src/boot/boot

--- tools/depsresolver-debian.sh
+++ tools/depsresolver-debian.sh
@@ -12,7 +12,26 @@
 #   6) Show goodbye message
 #   7) Wait for user to Close
 #########################################
+clear
 
-echo "Project Rhapsody Depenency Script\n"
-echo "\tOnly for Debian Systems\n"
-echo "\t(for now)"
+echo "**********************************"
+echo "Project Rhapsody Dependency Script"
+echo "**********************************"
+echo "     Only for Debian Systems"
+echo "            (for now)"
+echo "**********************************"
+
+date
+
+echo " "
+echo " "
+echo "This script will install the dependencies required to build and run Project Rhapsody."
+echo "        This will require sudo permissions to update and install packages."
+echo " "
+echo " "
+read -p "Press Enter to Continue"
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install git make bzip2 gcc libncurses-dev libssl-dev vim flex  bison bc cpio libelf-dev syslinux dosfstools libssl-dev
+
